028f4d05085f4f1c1f7083a37408be710eecb60c
cavinitommaso <cavinitommaso@gmail.com>
2026-01-15 10:35:50 +0100

    buttonLinls and swapSections

---
+++ script/linksTemplate.js
@@ -0,0 +1,28 @@
+window.linksTemplate = `
+<ul>
+    <li>
+    <div class="app-btn btn-link btn-home active" data-target="home">
+        <h2><i class="fa-solid fa-house"></i></h2>
+        <div>Home</div>
+    </div>
+    </li>
+    <li>
+    <div class="app-btn btn-link btn-about" data-target="about">
+        <h2><i class="fa-solid fa-user"></i></h2>
+        <div>About</div>
+    </div>
+    </li>
+    <li>
+    <div class="app-btn btn-link btn-resume" data-target="resume">
+        <h2><i class="fa-solid fa-folder-open"></i></h2>
+        <div>Resume</div>
+    </div>
+    </li>
+    <li>
+    <div class="app-btn btn-link btn-contact" data-target="contact">
+        <h2><i class="fa-solid fa-phone"></i></h2>
+        <div>Contact</div>
+    </div>
+    </li>
+</ul>
+`;
--- script/main.js
+++ script/main.js
@@ -1,1 +1,55 @@
-console.log("Hello World!");
+const ACTIVE = "active";
+
+let linkButtons, sections, burgerMenuCont;
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Inject Links Buttons
+  const headerNav = document.querySelector(".header-bottom");
+  if (headerNav) headerNav.innerHTML = window.linksTemplate;
+
+  const burgerBody = document.querySelector(".burger-menu-body");
+  if (burgerBody) burgerBody.innerHTML = window.linksTemplate;
+
+  linkButtons = document.querySelectorAll(".btn-link");
+  sections = document.querySelectorAll("main section");
+  burgerMenuCont = document.querySelector(".burger-menu-container");
+  const burgerBtnOpen = document.querySelector(".btn-burger");
+  const burgerBtnClose = document.querySelector(".burger-menu .btn-close");
+
+  // Sections
+  linkButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const targetId = btn.getAttribute("data-target");
+      swapSections(targetId);
+    });
+  });
+
+  // Burger Menu
+  burgerBtnOpen.addEventListener("click", openBurgerMenu);
+  burgerBtnClose.addEventListener("click", closeBurgerMenu);
+});
+
+function swapSections(targetId) {
+  const targetSection = document.getElementById(targetId);
+
+  if (targetSection) {
+    sections.forEach((s) => s.classList.remove(ACTIVE));
+    targetSection.classList.add(ACTIVE);
+
+    linkButtons.forEach((b) => b.classList.remove(ACTIVE));
+    const matchingButtons = document.querySelectorAll(`[data-target="${targetId}"]`);
+    matchingButtons.forEach((mBtn) => mBtn.classList.add(ACTIVE));
+
+    closeBurgerMenu();
+  }
+}
+
+function openBurgerMenu() {
+  burgerMenuCont.classList.add(ACTIVE);
+}
+
+function closeBurgerMenu() {
+  if (burgerMenuCont.classList.contains(ACTIVE)) {
+    burgerMenuCont.classList.remove(ACTIVE);
+  }
+}
